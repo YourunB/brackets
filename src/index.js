1e3e4b49e91f4e1a2f78aa42d5bdd152e75f767b
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-08-09 15:02:24 +0300

    feat: implemented the removal of identical brackets in a string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let check = str;
+  let index;
+  for (let l = 0; l < str.length / 2; l++) {
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if (check.indexOf(bracketsConfig[i].join("")) != -1) {
+        index = check.indexOf(bracketsConfig[i].join("")); 
+        check = check.split("");
+        check.splice(index, 2);
+        check = check.join("");
+      }
+    }
+  }
+  
+  if (check.length > 0) return false;
+  else return true;
 }
